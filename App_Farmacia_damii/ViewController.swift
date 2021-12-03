4911c360c84554d40d8ad979f10c9f15af0a3f8e
RaulMacOS <raulmacos@RaulMacOSs-iMac.local>
2021-12-02 22:07:15 -0500

    Initial Commit

---
+++ App_Farmacia_damii/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App_Farmacia_damii
+//
+//  Created by RaulMacOS on 12/2/21.
+//  Copyright © 2021 RaulMacOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
